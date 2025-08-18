0ca81ab636dfce0eb164a1a7bcac816ea2cd6abf
ardaasevinc <ardaasevinc@gmail.com>
2025-08-18 09:41:53 +0300

    güncelleme

--- resources/views/site/components/calltoaction.blade.php
+++ resources/views/site/components/calltoaction.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="mil-image-box">
             <div class="mil-image-frame">
-                <img src="site/assets/img/home-1/2.jpg" alt="img" class="mil-scale-img" data-value-1="1.20"
+                <img src="{{asset('site/assets/img/home-1/2.webp')}}" alt="img" class="mil-scale-img" data-value-1="1.20"
                     data-value-2="1">
                 <div class="mil-overlay"></div>
             </div>
--- resources/views/site/components/preloader.blade.php
+++ resources/views/site/components/preloader.blade.php
@@ -2,14 +2,14 @@
         <div class="mil-preloader">
             <div class="mil-preloader-animation">
                 <div class="mil-pos-abs mil-animation-1">
-                    <p class="mil-head1 mil-m1">Pioneering</p>
-                    <p class="mil-head1 mil-a2">Creative</p>
-                    <p class="mil-head1 mil-m1">Excellence</p>
+                    <p class="mil-head1 mil-m1">DESIGN</p>
+                    <p class="mil-head1 mil-a2">MEETS</p>
+                    <p class="mil-head1 mil-m1">FULL STACK</p>
                 </div>
                 <div class="mil-pos-abs mil-animation-2">
                     <div class="mil-reveal-frame">
                         <p class="mil-reveal-box"></p>
-                        <p class="mil-head1 mil-m1">pixy.com</p>
+                        <p class="mil-head1 mil-m1">ardasevinc.com.tr</p>
                     </div>
                 </div>
             </div>
