81c7bd0f0c27adfdb0eb76e8358f03e9666e6903
ardaasevinc <ardaasevinc@gmail.com>
2025-08-19 19:00:35 +0300

    cupdate

--- resources/views/site/components/menu.blade.php
+++ resources/views/site/components/menu.blade.php
@@ -41,7 +41,7 @@
 
                     </li>
                          <li class="mil">
-                        <a href="{{ route('site.cv.template') }}">CV OLUŞTUR</a>
+                        <a href="{{ route('site.cv.index') }}">CV OLUŞTUR</a>
 
                     </li>
                 </ul>
