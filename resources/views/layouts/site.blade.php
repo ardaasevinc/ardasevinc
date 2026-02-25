5d5f4d47a332fb99e4714fdbac6e9ae692f8b67f
ardaasevinc <ardaasevinc@gmail.com>
2026-02-25 16:02:52 +0300

    test

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -24,15 +24,36 @@
         <meta property="og:image" content="{{ asset('storage/' . $settings->og_image) }}">
     @endif
 
-    {{-- Google Analytics --}}
-    @if($settings?->google_analytics_code)
-        {!! $settings->google_analytics_code !!}
-    @endif
+ {{-- Google Analytics (GTAG.js) --}}
+@if(!empty($settings?->google_analytics_code))
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ $settings->google_analytics_code }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '{{ $settings->google_analytics_code }}');
+    </script>
+@endif
 
-    {{-- Facebook Pixel --}}
-    @if($settings?->facebook_pixel_code)
-        {!! $settings->facebook_pixel_code !!}
-    @endif
+{{-- Facebook Pixel --}}
+@if(!empty($settings?->facebook_pixel_code))
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '{{ $settings->facebook_pixel_code }}');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none" 
+             src="https://www.facebook.com/tr?id={{ $settings->facebook_pixel_code }}&ev=PageView&noscript=1"/>
+    </noscript>
+@endif
 
     {{-- Panelden Eklenen Özel Head Scriptleri (CSS vb.) --}}
     @if($settings?->header_scripts)
